adeef867a044999a6ce652ca1c43fb03875d2401
Yogendra Dawar <yogendradawar6@gmail.com>
2024-04-24 17:55:48 +0530

    linked the data

--- Hands_On_Practice/index.js
+++ Hands_On_Practice/index.js
@@ -16,6 +16,12 @@ app.get('/',function(req,res){
     
 })
 
+app.get('/file/:filename',function(req,res){
+    fs.readFile(`./files/${req.params.filename}`, "utf-8",function(err, filedata){
+        res.render('show',{filename: req.params.filename, filedata: filedata});
+    })
+})
+
 app.post('/create',function(req,res){
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
         res.redirect("/");
