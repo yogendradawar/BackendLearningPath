8046d3e9f0ce077ad0c9ddf36b2e700cddc06fcf
Yogendra Dawar <yogendradawar6@gmail.com>
2024-04-26 17:50:12 +0530

    model created

---
+++ CurdOperations/EjsAndServerSideRendering/app.js
@@ -0,0 +1,38 @@
+// #12
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const userModel = require('./models/user')
+
+app.set("view engine" , "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended : true}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',(req,res)=>{
+    res.render("index");
+});
+
+app.get('/read',async(req,res)=>{
+    let users = await userModel.find()
+    res.render("read" , {users});
+});
+
+app.get('/delete/:id',async(req,res)=>{
+    let users = await userModel.findOneAndDelete({_id: req.params.id});
+    res.redirect("/read");
+});
+
+app.post('/create', async (req,res)=>{
+    let {name , email , image } = req.body;
+     let createdUser = await userModel.create({
+        name,
+        email,
+        image,
+    });
+
+    res.redirect("/read");
+});
+
+app.listen(3000);
