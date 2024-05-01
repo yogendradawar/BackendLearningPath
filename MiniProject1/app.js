353c3f1dea6a014bdd6b9e32e875a52268353965
Yogendra Dawar <yogendradawar6@gmail.com>
2024-05-01 17:17:33 +0530

    login

--- MiniProject1/app.js
+++ MiniProject1/app.js
@@ -16,6 +16,20 @@ app.get('/',(req,res)=>{
     res.render("index");
 })
 
+app.get('/login',(req,res)=>{
+    res.render("login");
+})
+
+app.get('/profile' , isLoggedIn ,(req,res)=>{
+    console.log(req.user);
+    res.render("login");
+})
+
+app.get('/logout',(req,res)=>{
+    res.cookie("token","");
+    res.redirect("/login");
+})
+
 
 app.post('/register',async (req,res)=>{
     let {email, password, username, name, age} = req.body;
@@ -39,5 +53,30 @@ app.post('/register',async (req,res)=>{
         })
     })
 })
+
+app.post('/login',async (req,res)=>{
+    let {email, password} = req.body;
+
+    let user = await userModel.findOne({email});
+    if(!user) return res.status(500).send("Something Went Wrong");
+
+    bcrypt.compare(password,user.password,(err,result) => {
+        if(result) {
+            let token = jwt.sign({email : email , userid : user._id},"private");
+            res.cookie("token",token);
+            res.status(200).send("You can login");
+        }
+        else res.redirect("/login");
+    })
+})
+
+function isLoggedIn(req,res,next){
+    if(req.cookies.token === "") res.send("You must be logged in");
+    else{
+        let data =  jwt.verify(req.cookies.token, "private");
+        req.user = data;
+        next();
+    }
+}
 
 app.listen(3000);
