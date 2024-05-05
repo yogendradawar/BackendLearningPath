a6c8832fa54c5d56a2f409a32ed428117b6d951d
Yogendra Dawar <yogendradawar6@gmail.com>
2024-05-05 15:32:16 +0530

    multer use

--- MiniProject1/app.js
+++ MiniProject1/app.js
@@ -4,7 +4,11 @@ const userModel = require('./models/user');
 const postModel = require('./models/post');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const path = require('path');
+const multer = require('multer');
+
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -12,8 +16,31 @@ app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
 
 
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/images/uploads')
+    },
+    filename: function (req, file, cb) {
+        crypto.randomBytes(12, (err,bytes)=>{
+            const fn = bytes.toString("hex") + path.extname(file.originalname);
+            cb(null,fn);
+          })
+    }
+  })
+  
+const upload = multer({ storage: storage })
+
 app.get('/',(req,res)=>{
     res.render("index");
+})
+
+app.get('/test',(req,res)=>{
+    res.render("test");
+})
+
+app.post('/upload', upload.single("image"),(req,res)=>{
+    console.log(req.file);
 })
 
 app.get('/login',(req,res)=>{
